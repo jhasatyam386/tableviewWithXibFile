896ea14bc3de95f8b4a0f9112894592a731e4550
kirtanBaldha <kirtan9397@gmail.com>
2023-07-14 11:05:07 +0530

    Initial Commit

---
+++ tableviewWithXibFile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableviewWithXibFile
+//
+//  Created by R88 on 14/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
